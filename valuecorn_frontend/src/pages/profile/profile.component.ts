c7d167e71ef5a0109320dcc864d878eb366acaa2
Abhaya Subedi <Abhaya.Subedi@chs-corp.com>
2025-07-04 16:49:00 +0400

    adding workspace

--- valuecorn_frontend/src/pages/profile/profile.component.ts
+++ valuecorn_frontend/src/pages/profile/profile.component.ts
@@ -30,12 +30,12 @@ export class ProfileComponent {
     this.fetchValuations();
   }
   valuations: any[] = [];
-  selectedValuation: any = null;
-
+  selectedValuation: any = [];
+  selectedValuationDetails: any = [];
   
   fetchValuations() {
     this.valuationService.getAllValuations().subscribe({
-      next: (data) => {this.valuations = data;console.log(this.valuations)},
+      next: (data) => {this.valuations = data},
       error: (err) => console.error('Error fetching valuations', err)
     });
   }
@@ -48,7 +48,7 @@ export class ProfileComponent {
   fetchbyId(valuationid:number){
 this.valuationService.getValuationById(valuationid).subscribe({
 
-  next:(data)=>{this.selectedValuation=data;console.log(this.selectedValuation)},
+  next:(data)=>{this.selectedValuationDetails=data;},
   error:(err)=>console.error('Error fetching valuations', err)
   });
 }
